f705d218c1a0f3444e6002c3dc94a4e7767aedff
fdahnoun <fdahnoun@email.unc.edu>
2022-03-17 12:25:59 -0400

    updating files

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded( {extended: true} ))
-
-const logging= (req, res, next) => {
-    console.log(req.body.number)
-    next()
-}
 
 
+// app.use(express.json())
+// app.use(express.urlencoded( {extended: true} ))
 
-var port = 5000
+// const logging= (req, res, next) => {
+//     console.log(req.body.number)
+//     next()
+// }
 
-const server = app.listen(port, () => {
-    console.log('App is running on port %PORT%'.replace('%PORT%', port))
-})
 
-function coinFlip() {
-    return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
-}
 
-app.get('/app', (req, res) => {
-    res.status(200).end('OK')
-    res.type('text/plain')
+const PORT = process.env.PORT || 5000
+
+// Start an app server
+const server = app.listen(PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',PORT))
+});
+
+// function coinFlip() {
+//     return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+// }
+
+app.get('/app/', (req, res) => {
+// Respond with status 200
+    res.statusCode = 200;
+// respond with status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode)
     
 })
 
@@ -46,8 +52,8 @@ app.get('/app/flip', (req, res) => {
     res.status(200).json({ 'flip' : flip })
 })
 
+// Default response for any other request
 app.use(function(req, res){
-    res.status(404).send("Endpoint does not exist")
-    res.type("text/plain")
+    res.status(404).send('404 NOT FOUND')
 
 })
